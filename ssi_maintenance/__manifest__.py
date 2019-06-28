37dee1a797594759164885fad64b1f1bb6efb633
KrisQ <kquemener@yahoo.com>
2019-06-28 15:52:50 -0500

    see

--- ssi_maintenance/__manifest__.py
+++ ssi_maintenance/__manifest__.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
 
 #################################################################################
-# Systems Services, Inc. 
+# Systems Services, Inc.
 # Desc: To extend maintenance module to add fields to equipment
 #################################################################################
 
 {
-  "name"                 :  "SSI Maintenance Mods",
-  "summary"              :  "Add feilds to the equipment table",
-  "category"             :  "SSI",
-  "version"              :  "1.0",
-  "sequence"             :  1,
-  "author"               :  "Systems Services, Inc.",
-  "website"              :  "https://ssibtr.com",
-  "depends"              :  [
-                             'maintenance',
-                            ],
-  "data"                 :  [
-                              'views/ssi_maintenance.xml',
-                            ],
-  "application"          :  False,
-  "installable"          :  True,
-  "auto_install"         :  False,
+    "name":  "SSI Maintenance Mods",
+    "summary":  "Add feilds to the equipment table",
+    "category":  "SSI",
+    "version":  "1.0",
+    "sequence":  1,
+    "author":  "Systems Services, Inc.",
+    "website":  "https://ssibtr.com",
+    "depends":  [
+        'maintenance',
+    ],
+    "data":  [
+        'views/ssi_maintenance.xml',
+        'security/ir.model.access.csv',
+
+    ],
+    "application":  False,
+    "installable":  True,
+    "auto_install":  False,
 }
