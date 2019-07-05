eb7601bae91ff4600d0d9c0b205b8fa0de68cb9c
KrisQ <kquemener@yahoo.com>
2019-07-05 14:47:38 -0500

    see

--- ssi_jobs/models/jobs.py
+++ ssi_jobs/models/jobs.py
@@ -6,12 +6,37 @@ from odoo import models, fields
 
 
 class Jobs(models.Model):
-    _name = 'ssi.jobs'
+    _name = 'ssi_jobs'
     _description = 'Jobs'
     _order = "create_date,display_name desc"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
+    # LEFT
     name = fields.Char(required=True, index=True)
+    customer_id = fields.Many2one(
+        'res.partner', string='Customer', domain="[('customer', '=', 1)]")
+    objects = fields.Selection(
+        [('motor', 'Motor'), ('generator', 'Generator'), ('coil', 'Coil'), ('brake', 'Brake'), ('other', 'Other')], string='Object')
+    size = fields.Int(string='Size')
+    sizeUM = fields.Selection(
+        [('hp', 'Horsepower'), ('kw', 'Kilowatts'), ('lb-ft', 'Torque')], string='Size UM')
+    shaft = fields.Selection(
+        [('horizontal', 'Horizontal'), ('vertical', 'Vertical'), ('other', 'Other')], string='Shaft')
+    dimensions = fields.Float(string='Dimensions')
+
+    # RIGHT
+    ready_for_pickup = fields.Datetime(string='Ready for Pickup')
+    urgency = fields.Selection(
+        [('straight', 'Straight time'), ('straight_quote', 'Straight time quote before repair'), ('overtime', 'Overtime'), ('overtime_quote', 'Overtime quote before repair')], string='Urgency')
+    po_number = fields.Char(string='PO Number')
+    weight = fields.Float(string='Weight')
+    weightUM = fields.Selection(
+        [('lbs', 'pounds'), ('tons', 'tons'), ('kgs', 'kilograms')], string='Weight UM')
+    notes = fields.Text(string='Notes')
+    status = fields.Selection(
+        [('ready', 'Ready'), ('process', 'In Process'), ('done', 'Complete'), ('blocked', 'Blocked')], string='Status')
+
+    # OTHER
     partner_id = fields.Many2one(
         'res.partner', string='Partner', ondelete='restrict', required=True,
         domain=[('parent_id', '=', False)])
@@ -21,4 +46,4 @@ class Jobs(models.Model):
         'name_unique',
         'unique(name)',
         'This job name already exists in the system!'
-        )]
+    )]
