fe3c9af8d005cbd34e8deb2f6635ebd27ee4c5b6
KrisQ <kquemener@yahoo.com>
2019-07-08 14:35:09 -0500

    Many2one

--- ssi_jobs/models/so.py
+++ ssi_jobs/models/so.py
@@ -6,5 +6,5 @@ from odoo import api, fields, models, tools, _
 class MaintenanceEquipment(models.Model):
     _inherit = 'sale.order'
 
-    ssi_job_id = fields.One2many(
+    ssi_job_id = fields.Many2one(
         'ssi_jobs', string='Job')
