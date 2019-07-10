f62926dc66cae774662c848751470966ccb0327b
KrisQ <kquemener@yahoo.com>
2019-07-10 13:15:38 -0500

    see

--- ssi_jobs/models/jobs.py
+++ ssi_jobs/models/jobs.py
@@ -65,13 +65,12 @@ class Jobs(models.Model):
     # ACTION TO LEAD TO TABLE WITH SOs
     @api.one
     def action_view_estimates(self):
-        raise UserError(_('So far so good'))
-
-        # self.ensure_one()
-        # action = self.env.ref(
-        #     'sale_order_estimate_line_action').read()[0]
-        # action['domain'] = [('product_id', '=', self.id)]
-        # return action
+        # raise UserError(_('So far so good'))
+        self.ensure_one()
+        action = self.env.ref(
+            'sale_order_estimate_line_action').read()[0]
+        action['domain'] = [('product_id', '=', self.id)]
+        return action
 
     # <record id="stock_move_line_action" model="ir.actions.act_window">
     #         <field name="name">Product Moves</field>
