b07ab9ed2b43feb8bf8fdf934383f934ca8d4fc4
Kristenn Quemener <kristennquemener@Kristenns-MBP.attlocal.net>
2019-07-17 10:00:47 -0500

    see

--- ssi_jobs/models/jobs_stage.py
+++ ssi_jobs/models/jobs_stage.py
@@ -7,4 +7,4 @@ class JobsStage(models.Model):
     _description = 'Jobs Stage'
     
     name = fields.Char(string='Name')
-    display_name = fields.Char(string='Display Name', readonly=True)
+    display_name = fields.Char(string='Display Name', readonly=True, track_visibility="onchange")
