1804c3b811171d042ea4c9d00e33376040c61be0
Kristenn Quemener <kristennquemener@Kristenns-MBP.attlocal.net>
2019-07-18 15:12:44 -0500

    test

--- ssi_jobs/models/jobs_stage.py
+++ ssi_jobs/models/jobs_stage.py
@@ -8,3 +8,16 @@ class JobsStage(models.Model):
     
     name = fields.Char(string='Name')
     display_name = fields.Char(string='Display Name', readonly=True, track_visibility="onchange")
+
+    def _add_default_records_on_install(self):
+        default_records = [
+            'New Job',
+            'Inspection',
+            'Review',
+            'Quotation Sent',
+            'Awaiting Parts',
+            'Under Repair',
+            'Job Complete'
+        ]
+        for record in default_records:
+            self.create({'name': record})
