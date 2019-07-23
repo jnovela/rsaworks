7076eb50ccef9e02713e9f44c61935ef6e9ddc92
chadt63 <chadt@ssibtr.com>
2019-07-23 16:57:15 -0500

    Read Only 3

--- ssi_jobs/models/wc.py
+++ ssi_jobs/models/wc.py
@@ -7,7 +7,6 @@ class WC(models.Model):
     _inherit = 'mrp.workcenter.productivity'
 
     ssi_job_id = fields.Many2one('ssi_jobs', related='workorder_id.ssi_job_id', string='Job', readonly=True, store=False)
-	company_partner_id = fields.Many2one('res.partner', related='company_id.partner_id', string='Account Holder', readonly=True, store=False)
 #    ssi_job_id = fields.Many2one(
 #        'workorder_id.ssi_job_id', string='Job')
     # ssi_job_id = fields.Many2one(
