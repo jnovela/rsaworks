e6da4d17727a968f255345c9b1ede304fc3035a6
chadt63 <chadt@ssibtr.com>
2019-07-23 16:50:25 -0500

    Read Only

--- ssi_jobs/models/wc.py
+++ ssi_jobs/models/wc.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models, tools, _
 class WC(models.Model):
     _inherit = 'mrp.workcenter.productivity'
 
-    ssi_job_id = fields.Many2one(string='Job', related='workorder_id.ssi_job_id')
+    ssi_job_id = fields.Many2one('ssi_jobs', related='workorder_id.ssi_job_id', string='Job', readonly=True)
 #    ssi_job_id = fields.Many2one(
 #        'workorder_id.ssi_job_id', string='Job')
     # ssi_job_id = fields.Many2one(
