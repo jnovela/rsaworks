e630143a5dac5465deff4b5f462703ce1af98dfc
chadt63 <chadt@ssibtr.com>
2019-07-24 08:56:57 -0500

    Upodated job in attendnace

--- ssi_attendance/models/attendance.py
+++ ssi_attendance/models/attendance.py
@@ -9,11 +9,11 @@ class HrAttendance(models.Model):
         'open', 'Open'), ('approved', 'Approved')], default='open', track_visibility='onchange')
     attendance_lines = fields.One2many('hr.attendance.line', 'attendance_id', string='Attendance Lines', copy=True)
     job_id = fields.Many2one(
-        'x_jobs', ondelete='set null', string="Job", index=True)
+        'ssi_jobs', ondelete='set null', string="Job", index=True)
     workorder_id = fields.Many2one(
         'mrp.workorder', ondelete='set null', string="Work Order", index=True)
     labor_code_id = fields.Many2one(
-        'x_labor.codes', ondelete='set null', string="Labor Code", index=True)
+        'ssi_labor.codes', ondelete='set null', string="Labor Code", index=True)
 
 
     @api.one
