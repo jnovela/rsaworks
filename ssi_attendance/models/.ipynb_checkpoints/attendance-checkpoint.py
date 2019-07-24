c521aab02c6646a1832d149f1d8aa4946792ed2b
Chad T <chadt@ssibtr.com>
2019-07-24 14:21:38 +0000

    Update Mainfest

--- ssi_attendance/models/.ipynb_checkpoints/attendance-checkpoint.py
+++ ssi_attendance/models/.ipynb_checkpoints/attendance-checkpoint.py
@@ -8,12 +8,9 @@ class HrAttendance(models.Model):
     status = fields.Selection(string="Status", selection=[(
         'open', 'Open'), ('approved', 'Approved')], default='open', track_visibility='onchange')
     attendance_lines = fields.One2many('hr.attendance.line', 'attendance_id', string='Attendance Lines', copy=True)
-#    job_id = fields.Many2one(
-#        'ssi_jobs', ondelete='set null', string="Job")
-    workorder_id = fields.Many2one(
-        'mrp.workorder', ondelete='set null', string="Work Order", index=True)
-    labor_code_id = fields.Many2one(
-        'ssi_labor.codes', ondelete='set null', string="Labor Code", index=True)
+    job_id = fields.Many2one('ssi_jobs', ondelete='set null', string="Job", index=True)
+    workorder_id = fields.Many2one('mrp.workorder', ondelete='set null', string="Work Order", index=True)
+    labor_code_id = fields.Many2one('ssi_labor.codes', ondelete='set null', string="Labor Code", index=True)
 
 
     @api.one
