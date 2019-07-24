57787b21e6c3ee90db76adc295cc031cffb51f04
chadt63 <chadt@ssibtr.com>
2019-07-24 09:01:17 -0500

    Upodated job in attendnace 2

--- ssi_attendance/models/attendance.py
+++ ssi_attendance/models/attendance.py
@@ -9,7 +9,7 @@ class HrAttendance(models.Model):
         'open', 'Open'), ('approved', 'Approved')], default='open', track_visibility='onchange')
     attendance_lines = fields.One2many('hr.attendance.line', 'attendance_id', string='Attendance Lines', copy=True)
     job_id = fields.Many2one(
-        'ssi_jobs', ondelete='set null', string="Job", index=True)
+        'ssi_jobs', ondelete='set null', string="Job")
     workorder_id = fields.Many2one(
         'mrp.workorder', ondelete='set null', string="Work Order", index=True)
     labor_code_id = fields.Many2one(
@@ -46,11 +46,3 @@ class HrAttendanceLine(models.Model):
     worked_hours = fields.Float(string='Worked Hours', compute='_compute_worked_hours', store=True, readonly=True)
     status = fields.Selection(string="Status", selection=[(
         'open', 'Open'), ('approved', 'Approved')], default='open', track_visibility='onchange')
-
-#    job_id = fields.Many2one(
-#        'x_jobs', ondelete='set null', string="Job", index=True)
-#    workorder_id = fields.Many2one(
-#        'mrp.workorder', ondelete='set null', string="Work Order", index=True)
-#    labor_code_id = fields.Many2one(
-#        'x_labor.codes', ondelete='set null', string="Labor Code", index=True)
-
