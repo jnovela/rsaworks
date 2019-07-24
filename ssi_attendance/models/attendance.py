2cccd4e2982db60f193c4ec230f662bed9f0bbb0
chadt63 <chadt@ssibtr.com>
2019-07-24 08:40:15 -0500

    Updates to Attendance

--- ssi_attendance/models/attendance.py
+++ ssi_attendance/models/attendance.py
@@ -8,23 +8,29 @@ class HrAttendance(models.Model):
     status = fields.Selection(string="Status", selection=[(
         'open', 'Open'), ('approved', 'Approved')], default='open', track_visibility='onchange')
     attendance_lines = fields.One2many('hr.attendance.line', 'attendance_id', string='Attendance Lines', copy=True)
+    job_id = fields.Many2one(
+        'x_jobs', ondelete='set null', string="Job", index=True)
+    workorder_id = fields.Many2one(
+        'mrp.workorder', ondelete='set null', string="Work Order", index=True)
+    labor_code_id = fields.Many2one(
+        'x_labor.codes', ondelete='set null', string="Labor Code", index=True)
 
 
-    # @api.one
-    # def approve_attendance(self):
-        # data = {
-            # 'workorder_id': self.workorder_id.id,
-            # 'workcenter_id': self.workorder_id.workcenter_id.id,
-            # 'loss_id': 7,
-            # 'user_id': self.employee_id.user_id.id,
-            # 'date_start': self.check_in,
-            # 'date_end': self.check_out,
-            # 'x_studio_labor_codes': self.labor_code_id.id
-        # }
-        # self.write({
-            # 'status': 'approved'
-        # })
-        # self.env['mrp.workcenter.productivity'].sudo().create(data)
+    @api.one
+    def approve_attendance(self):
+        data = {
+            'workorder_id': self.workorder_id.id,
+            'workcenter_id': self.workorder_id.workcenter_id.id,
+            'loss_id': 7,
+            'user_id': self.employee_id.user_id.id,
+            'date_start': self.check_in,
+            'date_end': self.check_out,
+            'x_studio_labor_codes': self.labor_code_id.id
+        }
+        self.write({
+            'status': 'approved'
+        })
+        self.env['mrp.workcenter.productivity'].sudo().create(data)
 
 class HrAttendanceLine(models.Model):
     _name = "hr.attendance.line"
