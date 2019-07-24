f0bda56fbcbbf0dcca0040eb6772b4ad1cc4a912
chadt63 <chadt@ssibtr.com>
2019-07-24 08:08:00 -0500

    Add jobs module

---
+++ ssi_jobs/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+#################################################################################
+# Systems Services, Inc.
+# Desc: To extend maintenance module to add fields to equipment
+#################################################################################
+
+{
+    'name': 'Jobs',
+    'summary': "Adds jobs module to Odoo.",
+    'version': '1.0.1',
+    'category': 'SSI',
+    'author': 'Systems Services, Inc. '
+              'Chad Thompson ',
+    'website': 'https://ssibtr.com',
+    "depends":  [
+        'base',
+        'account',
+        'base',
+        'mrp',
+        'mrp_workorder',
+        'product',
+        'purchase',
+        'sale',
+        'stock',
+        'sale_timesheet',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/add_default_job_stages.xml',
+        'views/jobs.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
