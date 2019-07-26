0525d0e7a6eebbcee0fd055a48d0caccb8d5ac78
KrisQ <kquemener@yahoo.com>
2019-07-26 14:37:30 -0500

    add attendance

---
+++ ssi_attendance/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+#################################################################################
+# Systems Services, Inc.
+# Desc: To extend attendance module in order to create detail attendance records
+#################################################################################
+
+{
+    "name":  "SSI Attendance Mods",
+    "summary":  "Attendance Detail model and other modifications",
+    "category":  "SSI",
+    "version":  "1.0",
+    "sequence":  1,
+    "author":  "Systems Services, Inc.",
+    "website":  "https://ssibtr.com",
+    "depends":  [
+        'hr_attendance',
+    ],
+    "data":  [
+        'views/ssi_attendance.xml',
+        'views/ssi_attendance_report.xml',
+        'security/ir.model.access.csv',
+
+    ],
+    "application":  False,
+    "installable":  True,
+    "auto_install":  False,
+}
