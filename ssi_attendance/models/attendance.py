e74f88d5e130d70d9a6c9a594e9c4b831f061ad1
KrisQ <kquemener@yahoo.com>
2019-07-26 15:55:37 -0500

    see

--- ssi_attendance/models/attendance.py
+++ ssi_attendance/models/attendance.py
@@ -8,6 +8,10 @@ class HrAttendance(models.Model):
     status = fields.Selection(string="Status", selection=[(
         'open', 'Open'), ('approved', 'Approved')], default='open', track_visibility='onchange')
     attendance_lines = fields.One2many('hr.attendance.line', 'attendance_id', string='Attendance Lines', copy=True)
+    job_id = fields.Many2one(
+       'ssi_jobs', ondelete='set null', string="Job", index=True)
+    workorder_id = fields.Many2one(
+       'mrp.workorder', ondelete='set null', string="Work Order", index=True)
 
 
     @api.one
