44530695ddb8d590fde37edb7ba9085bb30b3c3f
chadt63 <chadt@ssibtr.com>
2019-07-31 13:09:46 -0500

    Cleanup Attendance

--- ssi_attendance/models/employee.py
+++ ssi_attendance/models/employee.py
@@ -24,28 +24,6 @@ class HrEmployeeCustom(models.Model):
             if entered_pin != self.pin:
                 return {'warning': _('Wrong PIN')}
         return self.attendance_action(next_action, job, wo, end)
-#         self.ensure_one()
-#         if not (entered_pin is None) or self.env['res.users'].browse(SUPERUSER_ID).has_group('hr_attendance.group_hr_attendance_use_pin') and (self.user_id and self.user_id.id != self._uid or not self.user_id):
-#             if entered_pin != self.pin:
-#                 return {'warning': _('Wrong PIN')}
-
-#         if not (job is None) and not (wo is None):
-#             last_attendance = self.env['hr.attendance'].search(
-#                 [('employee_id', '=', self.id)], limit=1)
-#             current_attendance = self.env['hr.attendance'].search([('employee_id', '=', self.id), ('check_out', '=', False)], limit=1)
-#             if last_attendance:
-#                 now = fields.Datetime.now()
-#                 last_attendance.sudo().write({'job_id': job})
-#                 last_attendance.sudo().write({'workorder_id': wo})
-#                 # last_attendance.sudo().write({'labor_code_id': lc})
-#                 current_attendance.sudo().write(
-#                         {'check_out': now})
-
-#                 if end == 'False':
-#                     last_attendance.sudo().write(
-#                         {'check_out': now})
-
-#         return self.attendance_action(next_action)
 
     @api.multi
     def attendance_action(self, next_action, job=None, wo=None, end=None):
@@ -67,36 +45,6 @@ class HrEmployeeCustom(models.Model):
         action_message['attendance'] = modified_attendance.read()[0]
         return {'action': action_message}
         
-#         """ Changes the attendance of the employee.
-#             Returns an action to the check in/out message,
-#             next_action defines which menu the check in/out message should return to. ("My Attendances" or "Kiosk Mode")
-#         """
-#         self.ensure_one()
-#         action_message = self.env.ref(
-#             'hr_attendance.hr_attendance_action_greeting_message').read()[0]
-#         action_message['previous_attendance_change_date'] = self.last_attendance_id and (
-#             self.last_attendance_id.check_out or self.last_attendance_id.check_in) or False
-#         action_message['employee_name'] = self.name
-#         action_message['barcode'] = self.barcode
-#         action_message['next_action'] = next_action
-
-# #         CUSTOM
-#         action_message['jobs'] = self.env['ssi_jobs'].search_read([])
-#         job_ids = []
-#         for job in action_message['jobs']:
-#             job_ids.append(job['id'])
-#         action_message['wos'] = self.env['mrp.workorder'].search_read(
-#             [('ssi_job_id', 'in', job_ids)])
-#         # action_message['lcs'] = self.env['x_labor.codes'].search_read([])
-
-#         if self.user_id:
-#             modified_attendance = self.sudo(
-#                 self.user_id.id).attendance_action_change()
-#         else:
-#             modified_attendance = self.sudo().attendance_action_change()
-#         action_message['attendance'] = modified_attendance.read()[0]
-#         return {'action': action_message}
-
     @api.multi
     def attendance_action_change(self, job=None, wo=None, end=None):
         """ Check In/Check Out action
@@ -145,41 +93,3 @@ class HrEmployeeCustom(models.Model):
                 raise exceptions.UserError(_('Cannot perform check out on %(empl_name)s, could not find corresponding check in. '
                     'Your attendances have probably been modified manually by human resources.') % {'empl_name': self.name, })
             return attendance
-
-#         """ Check In/Check Out action
-#             Check In: create a new attendance record
-#             Check Out: modify check_out field of appropriate attendance record
-#         """
-#         if len(self) > 1:
-#             raise exceptions.UserError(_('Cannot perform check in or check out on multiple employees.'))
-#         action_date = fields.Datetime.now()
-#         #here
-#         self.env['hr.attendance'].search([('employee_id', '=', self.id)], limit=1).sudo().write({'check_out': None})
-#         if self.attendance_state != 'checked_in':
-#             vals = {
-#                 'employee_id': self.id,
-#                 'check_in': action_date,
-#             }
-#             attendance = self.env['hr.attendance'].create(vals)
-#             #here
-#             self.env['hr.attendance.line'].sudo().create({
-#                 'employee_id': self.id,
-#                 'attendance_id': attendance.id,
-#                 'check_in': attendance.check_in,
-#                 # 'check_out': now,
-#                 # 'job_id': job,
-#                 # 'workorder_id': wo
-#             })
-
-#             return attendance
-#         else:
-#             attendance = self.env['hr.attendance'].search([('employee_id', '=', self.id), ('check_out', '=', False)], limit=1)
-#             #here
-#             attendance_l = self.env['hr.attendance.line'].search([('employee_id', '=', self.id), ('attendance_id', '=', attendance.id)], limit=1)
-#             if attendance:
-#                 # attendance.check_out = action_date
-#                 attendance_l.check_out = action_date
-#             else:
-#                 raise exceptions.UserError(_('Cannot perform check out on %(empl_name)s, could not find corresponding check in. '
-#                     'Your attendances have probably been modified manually by human resources.') % {'empl_name': self.name, })
-#             return attendance
