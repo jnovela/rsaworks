d11c302ac587ec97f977cd75877004eef9b2e9b8
KrisQ <kquemener@yahoo.com>
2019-08-06 09:27:01 -0500

    see

--- ssi_jobs/models/jobs.py
+++ ssi_jobs/models/jobs.py
@@ -82,7 +82,8 @@ class Jobs(models.Model):
                 'ssi_job_sequence') or _('New')
         res = super(Jobs, self).create(vals)
         name = res.name
-        group = self.env['account.group'].search([], limit=1).id
+        group = self.env['account.group'].search(
+            ['name', '=', 'Jobs'], limit=1).id
         partner = res.partner_id.id
         aa = self.env['account.analytic.account'].sudo().create(
             {'name': name, 'ssi_job_id': res.id, 'group_id': group, 'partner_id': partner})
