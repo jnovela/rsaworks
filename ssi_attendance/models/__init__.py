1c8cc41d32054b74b1845b2ff9f6e4eed5dfb4be
chadt63 <chadt@ssibtr.com>
2019-08-20 15:34:07 -0500

    Manual Merge with Test 2

--- ssi_attendance/models/__init__.py
+++ ssi_attendance/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-# Chad Test
 
+from . import resourceOvertime
 from . import attendance
 from . import attendanceReport
 from . import employee
-from . import resourceOvertime
