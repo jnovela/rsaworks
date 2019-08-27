4458a57b9c4abc633b5359e7cc6db01ddf5b9d16
chadt63 <chadt@ssibtr.com>
2019-08-27 11:00:09 -0500

    Add sq feet

--- ssi_maintenance/models/storage.py
+++ ssi_maintenance/models/storage.py
@@ -13,4 +13,6 @@ class Storage(models.Model):
     subscription_id = fields.Many2one('sale.subscription', string='Subscription')
     check_in = fields.Datetime(string='Check in')
     check_out = fields.Datetime(string='Check out')
+    equip_square_feet = fields.Float(string='Square Feet', related='equipment_id.square_feet')
+    equip_serial_no = fields.Char(string='Serial Number', related='equipment_id.serial_no')
     status = fields.Boolean(string='Status')
