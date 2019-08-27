27063f33861136c457e81380328c3da67fe97a1d
chadt63 <chadt@ssibtr.com>
2019-08-27 11:35:00 -0500

    Total Line on Sq Ft

--- ssi_maintenance/models/subscription.py
+++ ssi_maintenance/models/subscription.py
@@ -10,3 +10,14 @@ class SaleSubscription(models.Model):
     
 #     equip_id = fields.Char(string='Equip_id')
     storage_id = fields.One2many('storage', 'subscription_id', string='Storage')
+    square_foot_total = fields.Float(string='Square Foot Total', compute='_get_sqf_total')
+
+    # ACTIONS AND METHODS
+    @api.depends('storage_id')
+    def _get_sqf_total(self):
+        total = 0
+        for record in self:
+            for strg in record.storage_id:
+                total = total + strg.equip_square_feet
+            record.square_foot_total = total
+
