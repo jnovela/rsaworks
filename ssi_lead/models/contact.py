0ecd7e04b0c02cea288d8ab74f0ea06c87cc6019
chadt63 <chadt@ssibtr.com>
2019-09-13 10:22:22 -0500

    Add Project Manager

--- ssi_lead/models/contact.py
+++ ssi_lead/models/contact.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models, tools, _
 class Contact(models.Model):
     _inherit = 'res.partner'
 
+    project_manager_id = fields.Many2one('res.users', string='Project Manager', help='Project Manager from user table.')
     fax = fields.Char(string='Fax')
     opened_date = fields.Date(string='Opened Date')
     vendor_1099 = fields.Boolean(string='Vendor 1099')
