218eb52afc64009f7c0d29ea2489ca6976c095aa
chadt63 <chadt@ssibtr.com>
2019-10-24 07:51:53 -0500

    Updates to Check printing

--- osi_check_layout/__manifest__.py
+++ osi_check_layout/__manifest__.py
@@ -7,13 +7,11 @@
     'author': 'Open Source Integrators',
     'license': 'AGPL-3',
     'category': 'Localization',
-    'summary': 'Print Checks with Invoice Date, PO#, Supplier Invoice#, '
-               'Invoice Amount and Payment Amount',
+    'summary': 'Change check layout',
     'description': """
 This module prints checks on pre-printed or plain paper using top, middle or
 bottom format. Configure the layout in the company settings, and manage the
-numbering in journal settings. Check stubs have Invoice Date, Purchase Order
-number, Supplier Invoice Number, Invoice Amount and Payment amount.
+numbering in journal settings.
 
 Supported formats
 -----------------
@@ -21,7 +19,6 @@ Check on top, check on middle or check on bottom
     """,
     'website': 'http://www.opensourceintegrators.com',
     'depends' : [
-        'osi_accounting_enhancements',
         'l10n_us_check_printing',
     ],
     'data': [
@@ -30,7 +27,6 @@ Check on top, check on middle or check on bottom
         'report/osi_print_check_top.xml',
         'report/osi_print_check_middle.xml',
         'report/osi_print_check_bottom.xml',
-        'report/osi_print_check_stubs.xml',
         'views/osi_res_config_settings_views.xml',
     ],
     'installable': True,
