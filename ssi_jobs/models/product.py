7517e58ec8fbe06136ce57a552518b9b2b474c2a
chadt63 <chadt@ssibtr.com>
2019-12-09 15:41:35 -0600

    Add vendor rebate field

--- ssi_jobs/models/product.py
+++ ssi_jobs/models/product.py
@@ -13,3 +13,9 @@ class ProductCategory(models.Model):
     hide_on_print = fields.Boolean('Do Not Print', default=False)
     profit_center = fields.Selection(
         [('Disassembly', 'Disassembly'), ('Machine Shop', 'Machine Shop'), ('Winding', 'Winding'), ('Assembly', 'Assembly'), ('Field Services', 'Field Services'), ('New Product Sales', 'New Product Sales'), ('Storage', 'Storage'), ('Training', 'Training')], string='Profit Center')
+
+class ProductPricelistItem(models.Model):
+    _inherit = 'product.pricelist.item'
+
+    rebate_amount = fields.Float('Rebate Amount', digits=dp.get_precision('Product Price'))
+
