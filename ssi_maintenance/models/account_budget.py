003d31ebc1989843ded043ae33fddf1f53b461aa
chadt63 <chadt@ssibtr.com>
2020-02-07 10:10:09 -0600

    Budget Updates

--- ssi_maintenance/models/account_budget.py
+++ ssi_maintenance/models/account_budget.py
@@ -6,11 +6,18 @@ import datetime
 import requests
 
 
+class CrossoveredBudget(models.Model):
+    _inherit = "crossovered.budget"
+
+    budget_category = fields.Selection(
+        [('executive', 'Executive'), ('storage', 'Storage'), ('job', 'Job'), ('other', 'Other')], string='Budget Category')
+
 class CrossoveredBudgetLines(models.Model):
     _inherit = "crossovered.budget.lines"
 
     remaining_amount = fields.Monetary('Remaining Amount', compute='_compute_remaining',
         help="Amount remaining to spend.")
+    reference = fields.Char(string='Reference')
 
     @api.multi
     def _compute_remaining(self):
